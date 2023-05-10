5704f3e7966fac6068a6a1dff52b73f24b84e367
Spencer Smallwood <spencer.a.smallwood@gmail.com>
2023-05-09 22:59:37 -0700

    May 9th leave off

---
+++ lib/api_client.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class ApiClient {
+  const ApiClient._();
+  static const ApiClient _instance = ApiClient._();
+  factory ApiClient() => _instance;
+
+  final String baseUrl = 'https://www.omdbapi.com/?apikey=48d812d0&';
+
+  Future<void> findById(String id) async {
+    final http.Response response = await http.get(
+      Uri.parse('${baseUrl}i=$id'),
+    );
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> json = jsonDecode(response.body);
+      print(json);
+    }
+  }
+}
